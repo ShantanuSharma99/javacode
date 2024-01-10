4ff1c580ce5c92642950903ee929e55416526677
Shantanu Sharma <shantanusharma2015@gmail.com>
2024-01-10 15:53:03 +0530

    Commited 1st Code

---
+++ src/january/Jan.java
@@ -0,0 +1,11 @@
+package january;
+
+public class Jan {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("1st line of Java code added in git on 10 jan");
+
+	}
+
+}
